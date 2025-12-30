9788409209b14e1b1399812dbe788cdb08817925
Maksum <95537230+WebBlade@users.noreply.github.com>
2025-12-30 15:16:12 +0200

    add GenericRepository

---
+++ WishList.Application/Interfaces/Repositories/IGenericReporitory.cs
@@ -0,0 +1,12 @@
+﻿namespace WishList.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task Update(T entity);
+        Task<bool> Delete(int id);
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ WishList.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Application.Interfaces.Repositories;
+
+namespace WishList.Infrastructure.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly WishListDb _context;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(WishListDb dbContext)
+        {
+            _context = dbContext;
+            _dbSet = _context.Set<T>();
+        }
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+        public async Task Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _dbSet.Entry(entity).State = EntityState.Modified;
+        }
+        public async Task<bool> Delete(int id)
+        {
+            var entityToDelete = await _dbSet.FindAsync(id);
+
+            if (entityToDelete == null)
+            {
+                return false;
+            }
+
+            _dbSet.Remove(entityToDelete);
+
+            return true;
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
